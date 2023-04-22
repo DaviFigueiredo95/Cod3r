3194008b41a349e24731de76ec5818f1144604d9
Davi <davifigueiredo0195@gmail.com>
2023-04-21 21:30:57 -0300

    exercicio de string

---
+++ 21-04-2023/ex1.js
@@ -0,0 +1,14 @@
+// Maneira 1
+const cumprimentar = (nome) => {
+  return `Olá, ${nome}!`;
+};
+console.log(cumprimentar("Davi"));
+
+
+
+
+// Maneira 2
+const cumprimentarr = (name) => {
+    console.log(`Olá, ${name}!`);
+};
+cumprimentarr('Davi');

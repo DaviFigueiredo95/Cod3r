eb52cc467e1a1704662b67ff1284369a7987bc09
Davi <davifigueiredo0195@gmail.com>
2023-05-11 23:17:17 -0300

    comand select

---
+++ Banco de Dados/11-05-2023_5.sql
@@ -0,0 +1,5 @@
+SELECT * FROM ALUNOS
+SELECT NOME FROM ALUNOS
+SELECT * FROM ALUNOS WHERE NOME = 'HEMELLI'
+SELECT * FROM ALUNOS WHERE NOME LIKE "%DAVI%"
+# O operador acima significa que o nome pode buscar em qualquer parte do nome
